d7b8599965acefd2d87b84dfdf502ebfbab09b37
Quoc Truong Ngo <ngoquoc.work@gmail.com>
2024-05-03 21:49:19 -0700

    create html file for display and plug it to index.js

--- web projects/chat-app/index.js
+++ web projects/chat-app/index.js
@@ -6,8 +6,14 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 
-app.get('/', (req,res)=>{
-    res.send("Hello World from quoc")
+// app.get('/', (req,res)=>{
+//     res.send("Hello World from quoc")
+// })
+
+//pass the content of index.html to index.js
+//without this, we cannot see index.html when running index.js
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
 })
 
 server.listen(3000, ()=>{
