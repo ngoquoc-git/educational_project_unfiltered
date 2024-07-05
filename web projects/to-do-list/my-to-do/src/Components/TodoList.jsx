615a8d07bbb302ae46f0aabe129e487b3ac09151
Quoc Truong Ngo <ngoquoc.work@gmail.com>
2024-07-04 23:00:41 -0700

    re-create TodoList as a component

--- web projects/to-do-list/my-to-do/src/Components/TodoList.jsx
+++ web projects/to-do-list/my-to-do/src/Components/TodoList.jsx
@@ -1,7 +1,43 @@
 import React, {useState} from 'react';
 
 const TodoList = () => {
+    const[todoText, setTodo] = useState('');
+    const[todos, setTodos] = useState([])
 
+    const addTodo = () => {
+        if(todoText.trim()){
+            const newTodo = {text: todoText, completed: false};
+            setTodos([...todos, newTodo]);
+            setTodo('');
+        }
+    };
+
+    const removeTodo = (index) => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+
+    return(
+        <div>
+            <h1>To Do List</h1>
+                
+            <input type='text'                   
+                value = {todoText}
+                onChange = {(e) => setTodo(e.target.value)} />
+            <button onClick={addTodo}>Add Job</button>
+            
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>
+                        <span>{todo.text}</span>
+                        <button onClick={() => removeTodo(index)}>X</button>
+                    </li>
+                ))}
+            </ul>
+            
+        </div>
+    )
 }
 
 export default TodoList;
