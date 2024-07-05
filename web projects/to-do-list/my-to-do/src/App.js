cbe149a8794546b1fc286c4ea569af6a8edf60c9
Quoc Truong Ngo <ngoquoc.work@gmail.com>
2024-07-04 23:00:23 -0700

    remove TodoList feature from main App

--- web projects/to-do-list/my-to-do/src/App.js
+++ web projects/to-do-list/my-to-do/src/App.js
@@ -1,38 +1,12 @@
-import React, { useState } from 'react';
+
+import TodoList from './Components/TodoList.jsx'
 
 function App() {
-  const [todoText, setTodoText] = useState(''); // State for todo text
-  const [todos, setTodos] = useState([]); // State to store todo items
-
-  const addTodo = () => {
-    if (todoText.trim()) { // Check if text is not empty
-      const newTodo = { text: todoText, completed: false };
-      setTodos([...todos, newTodo]); // Update state with new todo
-      setTodoText(''); // Clear input field after adding
-    }
-  };
-
-  const deleteTodo = (index) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
-
   return (
     <div className="App">
-      <h1>Todo List</h1>
-      <input type="text" placeholder="Add a Todo" value={todoText} onChange={(e) => setTodoText(e.target.value)} />
-      <button onClick={addTodo}>Add</button>
-        <ul>
-          {todos.map((todo, index) => (
-            <li key={index}>
-              <span>{todo.text}</span>
-              <button onClick={() => deleteTodo(index)}>X</button>
-            </li>
-          ))}
-        </ul>
+      <TodoList />
     </div>
-  );
+  )
 }
 
 export default App;
